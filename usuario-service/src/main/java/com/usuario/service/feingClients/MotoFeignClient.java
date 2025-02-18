99c9d096e9e3edba5361311937520d42d07e1d98
LuisFer2500 <luisrodriguez2500@gmail.com>
2025-02-18 02:37:55 -0500

    gateway

--- usuario-service/src/main/java/com/usuario/service/feingClients/MotoFeignClient.java
+++ usuario-service/src/main/java/com/usuario/service/feingClients/MotoFeignClient.java
@@ -12,9 +12,9 @@ import java.util.List;
 @FeignClient(name = "moto-service")
 public interface MotoFeignClient {
 
-    @PostMapping("/moto")
+    @PostMapping("/api/v1/moto")
     public Moto saveMoto(@RequestBody Moto moto);
 
-    @GetMapping("/moto/usuario/{usuarioId}")
+    @GetMapping("/api/v1/moto/usuario/{usuarioId}")
     public List<Moto> getMotos(@PathVariable("usuarioId") int usuarioId);
 }
