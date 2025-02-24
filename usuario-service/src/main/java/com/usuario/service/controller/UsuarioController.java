fb92b1344eab8d836d0d5a58220fffae47705dea
LuisFer2500 <luisrodriguez2500@gmail.com>
2025-02-24 13:51:45 -0500

    circuit breaker

--- usuario-service/src/main/java/com/usuario/service/controller/UsuarioController.java
+++ usuario-service/src/main/java/com/usuario/service/controller/UsuarioController.java
@@ -4,7 +4,9 @@ import com.usuario.service.entity.Usuario;
 import com.usuario.service.models.Carro;
 import com.usuario.service.models.Moto;
 import com.usuario.service.service.UsuarioServicio;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -45,6 +47,7 @@ public class UsuarioController {
         return ResponseEntity.ok(newUsuario);
     }
 
+    @CircuitBreaker(name = "carrosCB", fallbackMethod = "fallBackGetCarros")
     @GetMapping("/carros/{usuarioId}")
     public ResponseEntity<List<Carro>> getCarros(@PathVariable("usuarioId") int usuarioId){
         Usuario usuario = usuarioServicio.getUsuarioById(usuarioId);
@@ -60,6 +63,7 @@ public class UsuarioController {
         return ResponseEntity.ok(carros);
     }
 
+    @CircuitBreaker(name = "carrosCB", fallbackMethod = "fallBackSaveCarro")
     @PostMapping("/carro/usuario/{usuarioId}")
     public ResponseEntity<Carro> saveCarro(@PathVariable("usuarioId") int id, @RequestBody Carro carro){
         Usuario usuario = usuarioServicio.getUsuarioById(id);
@@ -71,6 +75,7 @@ public class UsuarioController {
         return ResponseEntity.ok(nuevoCarro);
     }
 
+    @CircuitBreaker(name = "motosCB", fallbackMethod = "fallBackGetMotos")
     @GetMapping("/moto/{usuarioId}")
     public ResponseEntity<List<Moto>> getMotos(@PathVariable("usuarioId") int usuarioID){
         Usuario usuario = usuarioServicio.getUsuarioById(usuarioID);
@@ -84,6 +89,7 @@ public class UsuarioController {
         return ResponseEntity.ok(motos);
     }
 
+    @CircuitBreaker(name = "motosCB", fallbackMethod = "fallBackSaveMoto")
     @PostMapping("/moto/usuario/{usuarioId}")
     public ResponseEntity<Moto> saveMoto(@PathVariable("usuarioId") int usuarioId, @RequestBody Moto moto){
         Usuario usuario = usuarioServicio.getUsuarioById(usuarioId);
@@ -94,6 +100,7 @@ public class UsuarioController {
         return ResponseEntity.ok(usuarioServicio.saveMoto(usuarioId, moto));
     }
 
+    @CircuitBreaker(name= "todosCB", fallbackMethod = "fallBackGetTodos")
     @GetMapping("/todos/{usuarioId}")
     public ResponseEntity<Map<String, Object>> listarTodosVehiculos(@PathVariable("usuarioId") int usuarioId){
         Map<String,Object> resultado = usuarioServicio.getUsuarioAndVehiculo(usuarioId);
@@ -101,6 +108,25 @@ public class UsuarioController {
 
     }
 
+    //CircuitBreaker
+    private ResponseEntity<List<Carro>> fallBackGetCarros(@PathVariable("usuarioId") int id, RuntimeException exception){
+        return new ResponseEntity("El usuario" + id + "tiene los carros en el taller ", HttpStatus.OK);
+    }
 
+    private ResponseEntity<Carro> fallBackSaveCarro(@PathVariable("usuarioId") int id, @RequestBody Carro carro, RuntimeException exception){
+        return new ResponseEntity("El usuario " id + " no tiene saldo para comprar el carro ", HttpStatus.OK);
+    }
+
+    private ResponseEntity<List<Moto>> fallBackGetMotos(@PathVariable("usuarioId") int id, RuntimeException exception){
+        return new ResponseEntity("El usuario " + id + "tiene las motos en el taller ", HttpStatus.OK);
+    }
+
+    private ResponseEntity<Moto> fallBackSaveMoto(@PathVariable("usuarioId") int id, @RequestBody Moto moto, RuntimeException exception){
+        return new ResponseEntity("El usuario " + id + "no tiene saldo para comprar la moto ", HttpStatus.OK);
+    }
+
+    private ResponseEntity<Map<String, Object>> fallBackGetTodos(@PathVariable("usuarioId") int id){
+        return new ResponseEntity("El usuario "+ id + "tiene los vehiculos en el taller", HttpStatus.OK);
+    }
 
 }
